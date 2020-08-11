da5414b6d41b7c176164065e1782ec2d5b122bf0
nascono <aryhuh@gmail.com>
2020-08-11 03:23:51 +0300

    asd

---
+++ Turnike/Turnikea/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Runtime.Serialization.Formatters.Binary;
+using TurnikeClasses;
+namespace Turnike
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			
+		}
+		Logger logger = new Logger();
+		private float PictureHeight = 170f;
+		private int ItemMargin = 5;
+		List<SerialPort> girisportlari = new List<SerialPort>();
+		List<SerialPort> cikisportlari = new List<SerialPort>();
+
+		Ogrenci[] ogenciler;
+
+		
+		private void ListBox1_DrawItem(object sender, DrawItemEventArgs e)
+		{
+			
+			
+			ListBox lst = sender as ListBox;
+
+			if (e.Index == -1) { return; }
+			Ogrenci ogrencim = (Ogrenci)((object[])lst.Items[e.Index])[0];
+			Color renk = (Color)((object[])lst.Items[e.Index])[1];
+			string mesaj = (string)((object[])lst.Items[e.Index])[2];
+			Image foto = new Bitmap(133,171);
+			Graphics g = Graphics.FromImage(foto);
+			g.FillRectangle(new SolidBrush(Color.Black), 0, 0, foto.Width, foto.Height);
+			if (ogrencim.foto != "")
+			{
+				foto = Image.FromFile(ogrencim.foto);
+			}
+			
+			// Draw the background.
+			e.DrawBackground();
+
+			// Draw the picture.
+			float scale = PictureHeight / foto.Height;
+			RectangleF source_rect = new RectangleF(0, 0, foto.Width, foto.Height);
+			float picture_width = scale * foto.Width;
+			RectangleF dest_rect = new RectangleF(e.Bounds.Left + ItemMargin, e.Bounds.Top + ItemMargin, picture_width, PictureHeight);
+			e.Graphics.DrawImage(foto, dest_rect, source_rect, GraphicsUnit.Pixel);
+
+			// See if the item is selected.
+
+			// Find the area in which to put the text.
+			float x = e.Bounds.Left + picture_width + 3 * ItemMargin;
+			float y = e.Bounds.Top + ItemMargin;
+			float width = e.Bounds.Right - ItemMargin - x;
+			float height = e.Bounds.Bottom - ItemMargin - y;
+			RectangleF layout_rect = new RectangleF(x, y, width, height);
+
+
+			string txt = ogrencim.tam_ad + '\n' + ogrencim.numara + '\n' + mesaj;
+
+			e.Graphics.FillRectangle(new SolidBrush(renk), Rectangle.Round(layout_rect));
+			e.Graphics.DrawString(txt, new Font("Times New Roman", 24f), new SolidBrush(e.ForeColor), layout_rect);
+			if (lst.Items.Count == 6)
+			{
+				lst.Items.RemoveAt(0);
+			}
+		}
+
+		private void ListBox1_MeasureItem(object sender, MeasureItemEventArgs e)
+		{
+			e.ItemHeight = (int)(PictureHeight + 2 * ItemMargin);
+		}
+
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Add(new object[] { new Ogrenci("Ömer Fatih", 253, "asdfeghj", 2000, "",true,"asd") ,Color.Yellow,""});
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			StreamReader sr = new StreamReader("giris_portlari.txt");
+			string[] giris_portlari_txts = sr.ReadToEnd().Split(';');
+			sr.Close();
+			sr.Dispose();
+			StreamReader srr = new StreamReader("cikis_portlari.txt");
+			string[] cikis_portlari_txts = srr.ReadToEnd().Split(';');
+			srr.Close();
+			srr.Dispose();
+			foreach (string item in giris_portlari_txts)
+			{
+				//	girisportlari.Add(new SerialPort(item, 9600));
+				//	girisportlari[girisportlari.Count - 1].Open();
+				//	girisportlari[girisportlari.Count - 1].DataReceived += giris_geldi;
+			}
+			foreach (string item in cikis_portlari_txts)
+			{
+				//	cikisportlari.Add(new SerialPort(item, 9600));
+				//	cikisportlari[cikisportlari.Count - 1].Open();
+				//	cikisportlari[cikisportlari.Count - 1].DataReceived += cikis_geldi;
+			}
+			ogenciler = new Ogrenci().ogrencileri_getir();
+
+
+			listBox1.Width = this.Width - 50;
+			listBox1.Height = this.Height - 100;
+		}
+
+		private void giris_geldi(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+			SerialPort sp = sender as SerialPort;
+			byte[] gelen = new byte[sp.BytesToRead];
+			sp.Read(gelen, 0, sp.BytesToRead);
+			if (gelen != new byte[] {0x45,0x52,0x52,0x01,0x02,0x0D})
+			{
+				Ogrenci[] bulunanlar = (from x in ogenciler where x.card_number_with_footer_and_header == gelen select x).ToArray();
+				if (bulunanlar.Length>0)
+				{
+					Ogrenci ogrencim = bulunanlar[0];
+					DateTime gec_zamani = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 9, 0, 0, 0);
+					if (DateTime.Compare(DateTime.Now,gec_zamani)<0)
+					{
+						logger.new_log(new Log(ogrencim, "Giriş Yapıldı"));
+						sp.Write(new byte[] {0x52,0xC8},0,2);
+						listBox1.Items.Add(new object[] {ogrencim,Color.Green,"Giriş Yapıldı"});
+					}
+					else
+					{
+						logger.new_log(new Log(ogrencim, "Geç Giriş Yapıldı"));
+						sp.Write(new byte[] { 0x52, 0xC8 }, 0, 2);
+						listBox1.Items.Add(new object[] { ogrencim, Color.DarkGreen, "Geç Giriş Yapıldı" });
+					}
+				}
+				else
+				{
+					
+					listBox1.Items.Add(new object[] { new Ogrenci("Tanımsız Kart",0,"",0,"",false,"asd"), Color.Red, "" });
+				}
+			}
+		}
+		private void cikis_geldi(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+			SerialPort sp = sender as SerialPort;
+			byte[] gelen = new byte[sp.BytesToRead];
+			sp.Read(gelen, 0, sp.BytesToRead);
+			if (gelen != new byte[] { 0x45, 0x52, 0x52, 0x01, 0x02, 0x0D })
+			{
+				Ogrenci[] bulunanlar = (from x in ogenciler where x.card_number_with_footer_and_header == gelen select x).ToArray();
+				if (bulunanlar.Length > 0)
+				{
+					Ogrenci ogrencim = bulunanlar[0];
+					DateTime cikis_zamani = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 15, 50, 0, 0);
+					if (DateTime.Compare(DateTime.Now, cikis_zamani) >=0)
+					{
+						logger.new_log(new Log(ogrencim, "Çıkış Yapıldı"));
+						sp.Write(new byte[] { 0x52, 0xC8 }, 0, 2);
+						listBox1.Items.Add(new object[] { ogrencim, Color.Green, "Çıkış Yapıldı" });
+					}
+					else
+					{
+						if (ogrencim.izin)
+						{
+							logger.new_log(new Log(ogrencim, "İzinli Çıkış Yaptı(Yetkili)"));
+							sp.Write(new byte[] { 0x52, 0xC8 }, 0, 2);
+							listBox1.Items.Add(new object[] { ogrencim, Color.LightGreen, "İzinli Çıkış Yaptı(Yetkili)" });
+						}
+						else
+						{
+							logger.new_log(new Log(ogrencim, "Çıkmaya Çalıştı Ama Çıkamadı"));
+							listBox1.Items.Add(new object[] { ogrencim, Color.Red, "İzin Yok" });
+						}
+					}
+				}
+				else
+				{
+					listBox1.Items.Add(new object[] { new Ogrenci("Tanımsız Kart", 0, "", 0, "", false,"asd"), Color.Red, "","asd"});
+				}
+			}
+
+		}
+
+		private void Timer1_Tick(object sender, EventArgs e)
+		{
+			byte[] yazilcak = {0x02,0xff,0x96,0x04,0x0b,0x36,0x34,0x03};
+			foreach (SerialPort item in girisportlari)
+			{
+				item.Write(yazilcak, 0, yazilcak.Length);
+			}
+			foreach (SerialPort item in cikisportlari)
+			{
+				item.Write(yazilcak, 0, yazilcak.Length);
+			}
+		}
+
+		private void Button2_Click(object sender, EventArgs e)
+		{
+			//MessageBox.Show(logger.GetExecutingDirectoryName());
+
+		}
+	}
+}
---
+++ TurnikeClasses/TurnikeClasses/Class1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnikeClasses
+{
+	[Serializable]
+	public class Ogrenci
+	{
+		Seri seri = new Seri();
+		public string tam_ad, kart_no, foto;
+		public int numara, mezuniyet_tarihi;
+		public byte[] card_number_bytes = new byte[8];
+		public byte[] card_number_with_footer_and_header = new byte[18];
+		public bool izin = false;
+		public string telno = "";
+		public Ogrenci(string tam_ad, int numara, string kart_no, int mezuniyet_tarihi, string foto, bool izin, string telno)
+		{
+			this.tam_ad = tam_ad;
+			this.numara = numara;
+			this.kart_no = kart_no.ToUpper();
+			this.mezuniyet_tarihi = mezuniyet_tarihi;
+			this.foto = foto;
+			this.izin = izin;
+			this.telno = telno;
+			card_number_bytes = Encoding.UTF8.GetBytes(kart_no);
+			byte[] header = { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+			byte[] footter = { 0x20, 0x20, 0x20 };
+			Array.Copy(header, 0, card_number_with_footer_and_header, 0, header.Length);
+			Array.Copy(card_number_bytes, 0, card_number_with_footer_and_header, 7, card_number_bytes.Length);
+			Array.Copy(footter, 0, card_number_with_footer_and_header, 15, footter.Length);
+
+		}
+		public Ogrenci() { }
+		public Ogrenci[] ogrencileri_getir()
+		{
+			if (File.Exists("ogrenciler.ofas"))
+			{
+				StreamReader sr = new StreamReader("ogrenciler.ofas");
+				byte[] arrBytes = Convert.FromBase64String(sr.ReadLine());
+				sr.Close();
+				sr.Dispose();
+				Ogrenci[] obj = (Ogrenci[])seri.ByteArrayToObject(arrBytes);
+				return obj;
+			}
+
+			return new Ogrenci[] { };
+		}
+		public void ogrencileri_kaydet(Ogrenci[] ogrenciler)
+		{
+			if (!File.Exists("ogrenciler.ofas")){ File.Create("ogrenciler.ofas").Dispose(); }
+			StreamWriter sw = new StreamWriter("ogrenciler.ofas");
+			sw.WriteLine(Convert.ToBase64String(seri.ObjectToByteArray(ogrenciler)));
+			sw.Flush();
+			sw.Close();
+			sw.Dispose();
+		}
+
+		
+	}
+
+
+
+
+	public class Logger
+	{
+		Seri seri = new Seri();
+		public string file_name = "";
+		public string file_full_name = "";
+		public Logger()
+		{
+			string file_name = DateTime.Now.Day.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Year.ToString() + ".log";
+			string file_full_name = "logs/" + DateTime.Now.Day.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Year.ToString() + ".log";
+			this.file_full_name = file_full_name;
+			this.file_name = file_name;
+			if (!Directory.Exists("logs")) { Directory.CreateDirectory( "logs"); }
+			if (!File.Exists(file_full_name))
+			{
+				File.Create(file_full_name).Dispose();
+				StreamWriter sw = new StreamWriter(file_full_name);
+				sw.WriteLine(Convert.ToBase64String(seri.ObjectToByteArray(new List<Log>())));
+				sw.Flush();
+				sw.Close();
+				sw.Dispose();
+			}
+
+		}
+		
+		public void new_log(Log my_log)
+		{
+			StreamReader sr = new StreamReader(file_full_name);
+			List<Log> loglar = (List<Log>)seri.ByteArrayToObject(Convert.FromBase64String(sr.ReadLine()));
+			sr.Close();
+			sr.Dispose();
+
+			loglar.Add(my_log);
+
+			StreamWriter sw = new StreamWriter(file_full_name);
+			sw.WriteLine(Convert.ToBase64String(seri.ObjectToByteArray(loglar)));
+			sw.Flush();
+			sw.Close();
+			sw.Dispose();
+		}
+
+
+		public Log[] ogrencinin_loglarini_bul(Ogrenci ogrencim)
+		{
+			StreamReader sr = new StreamReader(file_full_name);
+			List<Log> loglar = (List<Log>)seri.ByteArrayToObject(Convert.FromBase64String(sr.ReadLine()));
+			sr.Close();
+			sr.Dispose();
+			return (from x in loglar where x.ogrenci == ogrencim select x).ToArray();
+		}
+		public Log[] olayin_loglarini_bul(string log)
+		{
+			StreamReader sr = new StreamReader(file_full_name);
+			List<Log> loglar = (List<Log>)seri.ByteArrayToObject(Convert.FromBase64String(sr.ReadLine()));
+			sr.Close();
+			sr.Dispose();
+			return (from x in loglar where x.olay == log select x).ToArray();
+		}
+		public Log[] tum_loglari_getir()
+		{
+			StreamReader sr = new StreamReader(file_full_name);
+			List<Log> loglar = (List<Log>)seri.ByteArrayToObject(Convert.FromBase64String(sr.ReadLine()));
+			sr.Close();
+			sr.Dispose();
+			return loglar.ToArray();
+		}
+		public Log[] ogrencinin_olayli_loglarini_bul(Ogrenci ogrencim, string olayim)
+		{
+			StreamReader sr = new StreamReader(file_full_name);
+			List<Log> loglar = (List<Log>)seri.ByteArrayToObject(Convert.FromBase64String(sr.ReadLine()));
+			sr.Close();
+			sr.Dispose();
+			return (from x in loglar where x.ogrenci == ogrencim && x.olay == olayim select x).ToArray();
+		}
+
+	
+	}
+
+
+
+
+
+
+
+
+	[Serializable]
+	public class Log
+	{
+		public Ogrenci ogrenci;
+		public string olay;
+		public DateTime zaman;
+		public Log(Ogrenci ogrenci, string olay)
+		{
+			this.ogrenci = ogrenci;
+			this.olay = olay;
+			this.zaman = DateTime.Now;
+		}
+
+	}
+
+
+	public class Seri
+	{
+		public byte[] ObjectToByteArray(Object obj)
+		{
+			if (obj == null)
+				return null;
+
+			BinaryFormatter bf = new BinaryFormatter();
+			MemoryStream ms = new MemoryStream();
+			bf.Serialize(ms, obj);
+
+			return ms.ToArray();
+		}
+
+		// Convert a byte array to an Object
+		public Object ByteArrayToObject(byte[] arrBytes)
+		{
+			MemoryStream memStream = new MemoryStream();
+			BinaryFormatter binForm = new BinaryFormatter();
+			memStream.Write(arrBytes, 0, arrBytes.Length);
+			memStream.Seek(0, SeekOrigin.Begin);
+			Object obj = (Object)binForm.Deserialize(memStream);
+
+			return obj;
+
+		}
+	} 
+}
